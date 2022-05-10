9573be5d875964c10c66664db71c9878f4eb0d34
Edward Martinez <cypun@users.noreply.github.com>
2022-05-10 11:00:43 -0400

    Create Hello_World.py

---
+++ Hello_World.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+firstWord = "Hello"
+secondWord = "World"
+space = " "
+
+print(firstWord,space,secondWord)
+print(firstWord+space+secondWord
+print("{} {}".format(firstWord,secondWord)
